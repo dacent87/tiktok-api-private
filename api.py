50da1dd7b027a6ba1614b460f1d7c4a5129035a2
Aleksey Novikov <gneo0194@gmail.com>
2026-05-17 13:02:26 +0300

    Update api.py

--- api.py
+++ api.py
@@ -28,5 +28,5 @@ hello tiktok api full methods x-gorgon x-argus x-ladon create accounts
 
 
 ### contact me
-**Contact for api :** [https://t.me/dacent53](https://t.me/dacent53)
-## telegram: @dacent53
+**Contact for api :** [https://t.me/dacent87](https://t.me/dacent87)
+## telegram: @dacent87
